1c2aa94904f5141ec491cce6083d8e4b7c085832
Costin Ion <Costinion@ymail.com>
2025-06-18 11:33:19 +0300

    added get all endpoint tests

--- internal/todos/handler_test.go
+++ internal/todos/handler_test.go
@@ -8,6 +8,8 @@ import (
 	"go.uber.org/zap"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -48,4 +50,91 @@ func TestHandler_Create(t *testing.T) {
 		assert.Equal(t, todoItem.Text, responseItem.Text)
 		assert.Equal(t, todoItem.Done, responseItem.Done)
 	}
+}
+
+func TestHandler_GetAll(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockService := NewMockService(ctrl)
+	e := echo.New()
+	todoItems := []ToDoItem{
+		{
+			Text: "go for a run",
+			Done: false,
+		},
+		{
+			Text: "go for a walk",
+			Done: false,
+		},
+		{
+			Text: "go for a jog",
+			Done: false,
+		},
+	}
+
+	logger := zap.NewNop().Sugar()
+	h := &endpointHandler{logger: logger, service: mockService, e: e}
+
+	t.Run("all items", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/todos/", nil)
+
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(req, rec)
+
+		mockService.
+			EXPECT().
+			GetAll(ctx.Request().Context(), PaginationDetails{}).
+			Return(todoItems, PaginationMetadata{
+				ResultCount: len(todoItems),
+				TotalCount:  len(todoItems),
+			}, nil).
+			Times(1)
+
+		if assert.NoError(t, h.getAll(ctx)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+
+			var responseData PaginatedResponse
+			err := json.Unmarshal(rec.Body.Bytes(), &responseData)
+			assert.NoError(t, err)
+
+			assert.Equal(t, responseData.Data, todoItems)
+			assert.Equal(t, responseData.Meta.ResultCount, len(todoItems))
+			assert.Equal(t, responseData.Meta.TotalCount, len(todoItems))
+		}
+	})
+
+	t.Run("paginated items", func(t *testing.T) {
+		paginationDetails := PaginationDetails{
+			Page:  1,
+			Limit: 2,
+		}
+
+		q := make(url.Values)
+		q.Set("page", strconv.Itoa(paginationDetails.Page))
+		q.Set("limit", strconv.Itoa(paginationDetails.Limit))
+		req := httptest.NewRequest(http.MethodGet, "/todos?"+q.Encode(), nil)
+
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(req, rec)
+
+		mockService.
+			EXPECT().
+			GetAll(ctx.Request().Context(), paginationDetails).
+			Return(todoItems[:2], PaginationMetadata{
+				ResultCount: 2,
+				TotalCount:  3,
+			}, nil).
+			Times(1)
+
+		if assert.NoError(t, h.getAll(ctx)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+
+			var responseData PaginatedResponse
+			err := json.Unmarshal(rec.Body.Bytes(), &responseData)
+			assert.NoError(t, err)
+
+			assert.Equal(t, responseData.Data, todoItems[:2])
+			assert.Equal(t, responseData.Meta.ResultCount, 2)
+			assert.Equal(t, responseData.Meta.TotalCount, 3)
+		}
+	})
 }
